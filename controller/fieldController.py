03af6fac04ea6191b1e851b815c8a86fe901e62e
Ismail Rohaga <azh.zhafir@gmail.com>
2019-10-04 08:12:14 +0700

    CRUD FieldController for field.txt new method available: -get_with_id()  > return field with id
    -update_line(args, index)
     > update line with args by specified index
    
    next development:
    -write_line_index(index)
     > write line with index specified, to replace previous index and rewrite //can be achieved using insert()

--- controller/fieldController.py
+++ controller/fieldController.py
@@ -12,9 +12,18 @@ def get_all():
     return open(path, "r").read()
 
 
+def get_with_id():
+    wid = []
+    iid = 1
+    for i in get_all().split():
+        wid.append(str(iid)+'. '+i)
+        iid = iid + 1
+    return wid
+
+
 def get_line(index: int):
     line = get_all().split()
-    return line[index]
+    return line[index-1]
 
 
 def write_line(args):
@@ -23,9 +32,23 @@ def write_line(args):
     write_file.close()
 
 
+def update_line(args, index: int):
+    line = get_all().split()
+    line[index-1] = args
+    write_file = open(path, "w")
+    for i in line:
+        length = len(line)
+        if line[length - 1] == i:
+            write_file.write(i)
+        else:
+            write_file.write(i + "\n")
+    write_file.close()
+    return "updated"
+
+
 def delete_line(index: int):
     line = get_all().split()
-    line.remove(line[index])
+    line.remove(line[index-1])
     write_file = open(path, "w")
     for i in line:
         length = len(line)
