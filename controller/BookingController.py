33c9220cff45f06c79b6d333896b321683ca1b06
pratamacalvin5 <pratamacalvin5@gmail.com>
2019-12-10 17:53:16 +0700

    Create controller

---
+++ controller/BookingController.py
@@ -0,0 +1,29 @@
+import helper.CrudHelper as CrudHelper
+import model.model as model
+
+model = model.Booking
+helper = CrudHelper.CrudHelper(model.name)
+
+
+class BookingController:
+    def __init__(self):
+        self.data = helper.get_all()
+
+    def get(self):
+        return self.data
+
+    @staticmethod
+    def create(args):
+        return helper.create(model.attr, args)
+
+    @staticmethod
+    def read(mid):
+        return helper.read(mid)
+
+    @staticmethod
+    def update(mid, args):
+        return helper.update(mid, model.attr, args)
+
+    @staticmethod
+    def remove(mid):
+        return helper.delete(mid)
